3c382bb807f35b785a64055aa777c5853dfa5dbf
Praveen Jadhav <155697218+praveenjadhav1510@users.noreply.github.com>
2025-08-05 01:10:14 +0530

    Update firebase.js
    Remove firebase api keys

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,6 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCiZvhDuRfeafVbf7rsL0GvSPPKn_Hl4AI",
-  authDomain: "portfolio-b0215.firebaseapp.com",
-  projectId: "portfolio-b0215",
-  storageBucket: "portfolio-b0215.firebasestorage.app",
-  messagingSenderId: "278355912086",
-  appId: "1:278355912086:web:54bd0111ca2f6b4a4e29b7",
-  measurementId: "G-KGNP7DVVEK",
 };
 
 // Initialize Firebase
